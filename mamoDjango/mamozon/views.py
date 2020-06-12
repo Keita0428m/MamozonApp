4819ea1556cfbf86baf134f80b758c66d16d3342
Keita39 <takoyaki.nao39@gmail.com>
2020-06-12 23:13:19 +0900

    商品一覧ページ追加。 商品情報の画像、Modelを追加

--- mamoDjango/mamozon/views.py
+++ mamoDjango/mamozon/views.py
@@ -2,6 +2,18 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.views import generic 
+from .models import * 
 
 class Lp(generic.TemplateView):
     template_name = 'mamozon/lp.html'
+
+class ItemList(generic.ListView):
+    model = Product
+    template_name = 'mamozon/item_list.html'
+
+    def get_queryset(self):
+        products = Product.objects.all()
+        if 'q' in self.request.GET and self.request.GET['q'] != None:
+            q = self.request.GET['q']
+            products = products.filter(name__icontains = q)
+        return products
